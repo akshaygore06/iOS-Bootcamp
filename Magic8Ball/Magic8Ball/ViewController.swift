0fd8f1a3da14f1e54f2a1e0a2d642512e0087e0e
Akshay Gore <agore1@mail.csuchico.edu>
2018-04-06 00:40:44 -0700

    Update the magic 8 ball image on button click

--- Magic8Ball/Magic8Ball/ViewController.swift
+++ Magic8Ball/Magic8Ball/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    
+    @IBOutlet weak var imageView: UIImageView!
 
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        updateBallImage()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateBallImage()
+    }
+    
+    func updateBallImage(){
+        var randomBallNumber = Int(arc4random_uniform(5))
+        imageView.image = UIImage(named: ballArray[randomBallNumber])
     }
 
     override func didReceiveMemoryWarning() {
